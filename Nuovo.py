34ef7fc61084b2e85c1846b02103e8ede06ccde5
MicheleMatteucci <95908837+MicheleMatteucci@users.noreply.github.com>
2021-12-10 11:44:26 +0100

    Add files via upload

---
+++ Nuovo.py
@@ -0,0 +1,8 @@
+# il programma fa semplici operazioni ho numeri e stringhe
+import math as m
+
+pi = 3.1415             # definisco un'approssimazione di pi greco
+
+print("pi greco =", pi, "...")      # scrivo la costante definita e alcune semplici operazioni
+print("pi greco ~", str(pi)[0:4])
+print("sin(pi) =", m.sin(pi), "~ 0" )
